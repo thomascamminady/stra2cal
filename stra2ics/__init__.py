35fa450907c584d704b2b400130284c3814c252c
Thomas Camminady <0milieux_member@icloud.com>
2024-05-01 08:21:04 +0200

    Remove makefile.

--- stra2ics/__init__.py
+++ stra2ics/__init__.py
@@ -1,4 +1,1 @@
 """.. include:: ../README.md."""
-__author__ = """Thomas Camminady"""
-__email__ = """0milieux_member@icloud.com"""
-__version__ = """0.1.0"""
--- tests/test_stra2ics.py
+++ tests/test_stra2ics.py
@@ -1,8 +1,2 @@
-"""Tests for `stra2ics` package."""
-
-
-from stra2ics import __version__
-
-
-def test_version() -> None:
-    assert __version__ == "0.1.0"
+def test_true():
+    assert True
